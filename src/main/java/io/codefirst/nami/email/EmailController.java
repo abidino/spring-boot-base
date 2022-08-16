48ed9b935f1651dfdb3c6bbfc04ab09b3bfd9e95
abidino <abidinozd@gmail.com>
2022-08-16 15:45:16 +0300

    optimize import

--- src/main/java/io/codefirst/nami/email/EmailController.java
+++ src/main/java/io/codefirst/nami/email/EmailController.java
@@ -1,7 +1,6 @@
 package io.codefirst.nami.email;
 
 import io.codefirst.nami.app.NamiConstant;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
