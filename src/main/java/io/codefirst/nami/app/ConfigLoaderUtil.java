e6c50d989e3e87b77069317a9a0ec5dd67ccae49
abidino <abidinozd@gmail.com>
2022-08-24 20:15:58 +0300

    utility struct changed to interface

--- src/main/java/io/codefirst/nami/app/ConfigLoaderUtil.java
+++ src/main/java/io/codefirst/nami/app/ConfigLoaderUtil.java
@@ -1,12 +1,8 @@
 package io.codefirst.nami.app;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+public interface ConfigLoaderUtil {
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class ConfigLoaderUtil {
-
-    public static String getProperty(String key) {
+     static String getProperty(String key) {
         return SpringContext.getAppContext().getEnvironment().getProperty(key);
     }
 }
